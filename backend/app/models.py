5cea23e462e322bff516a5a41744773fedade183
lamerous <lam3r0us@gmail.com>
2026-01-27 17:56:24 +0300

    fix (models): made email address not required

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, CheckConstraint
+from sqlalchemy import Column, Integer, BigInteger, String, ForeignKey, CheckConstraint, DateTime, func
 from sqlalchemy.orm import relationship, declarative_base
 from app.database import Base
 
@@ -6,9 +6,11 @@ class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True, nullable=False)
-    email = Column(String, unique=True, index=True, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=True)
     sex = Column(String, nullable=False, default="secret")
-    hashed_password = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=True)
+    telegram_id = Column(BigInteger, unique=True, nullable=True, default=None)
+    created_at = Column(DateTime, server_default=func.now())
 
     __table_args__ = (
         CheckConstraint("sex IN ('male', 'female', 'secret')", name="check_sex_types"),
@@ -18,4 +20,6 @@ class Room(Base):
     __tablename__ = "rooms"
     id = Column(String, primary_key=True)
     owner_id = Column(Integer, ForeignKey("users.id"))
+    member_id = Column(Integer, ForeignKey("users.id"))
     status = Column(String, default="active")
+    created_at = Column(DateTime, server_default=func.now())
