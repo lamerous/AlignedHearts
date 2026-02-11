f47b3e58c42dab414467d56f198637cf540e0365
lamerous <lam3r0us@gmail.com>
2026-02-11 17:15:32 +0300

    Initial commit

---
+++ backend-spring/src/main/java/com/alignedhearts/ah_spring/AlignedHeartsApplication.java
@@ -0,0 +1,13 @@
+package com.alignedhearts.ah_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlignedHeartsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlignedHeartsApplication.class, args);
+	}
+
+}
---
+++ backend-spring/src/test/java/com/alignedhearts/ah_spring/AlignedHeartsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alignedhearts.ah_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlignedHeartsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
